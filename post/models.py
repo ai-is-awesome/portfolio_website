fcf20b0a1e1749636f8d6c19f9b6ce54d20e1bed
Piyush <irock0093@gmail.com>
2020-05-18 05:16:54 +0530

    Created models, views, added support for images debuggin rn

--- post/models.py
+++ post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Project(models.Model):
+
+	title = models.CharField(max_length = 200)
+	description = models.TextField()
+	technology = models.CharField(max_length = 70)
+	image = models.ImageField(upload_to = 'images/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- post/views.py
+++ post/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
 
 
 def index(request):
-	return render(request, 'post/index.html')
+	projects = Project.objects.all()
+	context = {'projects': projects}
+	return render(request, 'post/index.html', context)
+
+
+def project_detail(request, pk):
+	project = Project.objects.get(id = pk)
+	context = {'project' : project}
+	return render(request, 'post/project_detail.html', context)
+
