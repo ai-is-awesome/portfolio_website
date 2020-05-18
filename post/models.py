da8c5179d74890591eed62a11238ce0e5306f9ee
Piyush <irock0093@gmail.com>
2020-05-19 01:14:04 +0530

    Added bootstrap, css file, static file support, images and the basic setup of website is done now

--- post/models.py
+++ post/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
 
 	title = models.CharField(max_length = 200)
 	description = models.TextField()
-	technology = models.CharField(max_length = 70)
+	technology = models.CharField(max_length = 250)
 	image = models.ImageField(upload_to = 'images/')
 
 
